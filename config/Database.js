c4999cd1d468db5e321ff063eac0f08d50f51363
wisnusunadi <barto7773@gmail.com>
2024-11-28 13:43:04 +0700

    fix vercel

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config();
-const db = new Sequelize(process.env.DB_HOSTNAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-    host : "localhost",
+const db = new Sequelize(process.env.DB_DATABASE,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+    host :process.env.DB_HOSTNAME,
     dialect : "mysql",
     define: {
         timestamps: false
